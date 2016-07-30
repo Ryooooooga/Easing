5af9ea2151fba998c71a0d885a4deea19e9368cb
Ryoga <ryoga_314@yahoo.co.jp>
2016-07-30 20:43:50 +0900

    :new: add Easing.hpp

---
+++ Bell/Easing.hpp
@@ -0,0 +1,12 @@
+//=====================================================================
+// Copyright (c) 2016 Ryooooooga.
+// https://github.com/Ryooooooga
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//=====================================================================
+
+#pragma once
+
+#include "Easing/Easing.hpp"
